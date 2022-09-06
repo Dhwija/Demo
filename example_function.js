6a909c416b26b1fd08af607b9c2efc69d428ac72
Dhwija <dhwija7@gmail.com>
2022-09-06 14:50:50 -0700

    1commit

---
+++ example_function.js
@@ -0,0 +1,17 @@
+function fact(num) //function signature
+{
+    res=1;
+for(i=1;i<=num;i++)
+     {
+      if(num%i==0)
+       {
+        res=i*res;
+       }
+     
+   }// functin body 
+return res;
+}
+console.log(fact (12)) //function call
+console.log(fact (6))
+console.log(fact (5))
+console.log(fact (8))
